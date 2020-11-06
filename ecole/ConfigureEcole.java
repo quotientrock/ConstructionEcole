43e1ff07898c0699b45c3aebafe322af62be7f15
quotientrock <40922916+quotientrock@users.noreply.github.com>
2020-11-06 16:18:51 +0100

    rearrangement en vrai class avec methode du menu.

--- ecole/ConfigureEcole.java
+++ ecole/ConfigureEcole.java
@@ -3,6 +3,14 @@ package ecole;
 
 public class ConfigureEcole {
 
+	public static void main(String[] args) {
+		
+		Communaute com=configureCommunaute();
+		configureRoutes(com);
+		configureEcoles(com);
+		
+	}
+	
 
 
 }
--- ecole/Menu.java
+++ ecole/Menu.java
@@ -4,14 +4,24 @@ import java.util.Scanner;
 
 public class Menu {
 
-	public static void main(String[] args) {
-		int choix=1;
-		char ville1=' ', ville2=' ';
+	
+	
+	public static Communaute configureCommunaute() {
+
 		System.out.println("nombre de villes(entre 1 et 25) : ");
 		Scanner sc = new Scanner(System.in);
 		int nb = sc.nextInt();
 		Communaute com= new Communaute(nb);
 		com.afficheEcoleVille(nb);
+		sc.close();
+		return com;
+	}
+	
+	public static void configureRoutes(Communaute com) {
+		int nb=com.getVilles().length;
+		int choix=1;
+		char ville1=' ', ville2=' ';
+		Scanner sc = new Scanner(System.in);
 		while(nb<26 && choix!=2) {
 			System.out.println(" 1 : ajouter une route ");
 			System.out.println(" 2 : Fin ");
@@ -31,6 +41,13 @@ public class Menu {
 				break;
 			}
 		}
+		sc.close();
+	}
+	public static void configureEcoles(Communaute com,int nb) {
+		int nb=com.getVilles().length;
+		Scanner sc = new Scanner(System.in);
+		int choix=1;
+		char ville1=' ', ville2=' ';
 		while(choix>=1 && choix<=2) {
 			com.afficheEcoleVille(nb);
 			System.out.println(" 1 : ajouter une ecole ");
@@ -54,5 +71,4 @@ public class Menu {
 		}
 		sc.close();
 	}
-
 }
